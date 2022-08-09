9d9e619f3ec7df51ca6f1df0d54e4f8309af506e
Наталья Кадикина <natalya92filipp@gmail.com>
2022-08-09 08:51:40 +0300

    Исправила замечание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,8 +3,7 @@ package ru.skypro;
 import java.time.LocalDate;
 
 public class Main {
-    public static void checkYear(int year)
-    {
+    public static void checkLeapYear(int year) {
         if ((year % 4 == 0) && (year % 100 != 0)) {
             System.out.println(year + " год является високосным");
         }
@@ -13,26 +12,30 @@ public class Main {
         }
     }
 
-    public static void checkPhone(int clientOs, int clientDeviceYear)
-    {
+    public static void checkPhone(int clientOs, int clientDeviceYear) {
         int currentYear = LocalDate.now().getYear();
-        if(clientOs == 0 && clientDeviceYear > currentYear) {
+        if(clientOs == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        else if (clientOs == 1 && clientDeviceYear > currentYear) {
+        else if (clientOs == 0 && clientDeviceYear >= currentYear) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        }
+        else if (clientOs == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (clientOs == 1 && clientDeviceYear >= currentYear) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
     }
 
-    public static int distance(int deliveryDistance)
-    {
-        if (deliveryDistance <= 20) {
+    public static int deliveryDistance(int distance) {
+        if (distance <= 20) {
             return  1;
         }
-        else if (deliveryDistance <= 60) {
+        else if (distance <= 60) {
             return  2;
         }
-        else if (deliveryDistance < 100) {
+        else if (distance < 100) {
             return  3;
         }
         return -1;
@@ -40,13 +43,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// Задание 1
-        checkYear(2022);
+        checkLeapYear(2022);
 
         //Задание 2
         checkPhone(1, 2023);
 
         //Задание 3
-        int day = distance(45);
+        int day = deliveryDistance(45);
         System.out.println("Потребуется дней: "+day);
     }
 }
